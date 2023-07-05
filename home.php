bb366192810edb8d51186d7be9a888deeeaba76e
jayaprasad356 <jayaprasad356@gmail.com>
2023-07-06 01:21:40 +0530

    e

--- home.php
+++ home.php
@@ -59,7 +59,7 @@ include "header.php";
                         <a href="users.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
-                <div class="col-lg-3 col-xs-6">
+                <!-- <div class="col-lg-3 col-xs-6">
                     <div class="small-box bg-aqua">
                         <div class="inner">
                             <h3><?php
@@ -72,7 +72,7 @@ include "header.php";
                         <a href="analysis.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                         
                     </div>
-                </div>
+                </div> -->
                 <!-- <div class="col-lg-3 col-xs-6">
                     <div class="small-box bg-aqua">
                         <div class="inner">
