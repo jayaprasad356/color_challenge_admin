a7aac68174b8bcefe16fd6bcff7458e3f9ed6624
jayaprasad356 <jayaprasad356@gmail.com>
2023-07-07 00:28:19 +0530

    settings

--- api/settings.php
+++ api/settings.php
@@ -12,19 +12,24 @@ include_once('../includes/crud.php');
 $db = new Database();
 $db->connect();
 
-
+$user_id = (isset($_POST['user_id']) && !empty($_POST['user_id'])) ? $db->escapeString($_POST['user_id']) : '';
+            
 $sql = "SELECT * FROM settings WHERE id=1";
 $db->sql($sql);
 $res= $db->getResult();
 $num = $db->numRows($res);
 
 if ($num >= 1){
-    // foreach ($res as $row) {
-    //     $temp['id'] = $row['id'];
-    //     $temp['name'] = $row['name'];
-    //     $temp['code'] = $row['code'];
-    //     $rows[] = $temp;
-    // }
+    if($user_id != ''){
+        $sql = "SELECT * FROM users WHERE id=$user_id";
+        $db->sql($sql);
+        $ures= $db->getResult();
+        if($ures[0]['earn'] != 0){
+            $res[0]['refer_coins'] = 10;
+        }
+
+    }
+
     $response['success'] = true;
     $response['message'] = "Settings Listed Successfully";
     $response['data'] = $res;
