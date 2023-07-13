88b134653c96c75ebca968e0e3b85be340f8004b
jayaprasad356 <jayaprasad356@gmail.com>
2023-07-13 14:01:13 +0530

    register

--- includes/crud.php
+++ includes/crud.php
@@ -5,7 +5,7 @@ define('DOMAIN_URL', 'https://democc.graymatterworks.com/'); /* chnage to your d
 define('ALLOW_MODIFICATION', 1);
 define('JWT_SECRET_KEY', '12345678'); /* chnage to your secret key here set it to anything you like*/
 define('MAIN_REFER', 'CMDS');
-define('MIN_WITHDRAWAL', 50);
+define('MIN_WITHDRAWAL', 10);
 define('REFER_COINS', 20);
 class Database
 {
