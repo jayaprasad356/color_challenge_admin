0bbcae258e8ad5af40a5c025d3f698cc53024de9
jayaprasad356 <jayaprasad356@gmail.com>
2023-07-14 12:41:50 +0530

    home

--- home.php
+++ home.php
@@ -77,20 +77,23 @@ include "header.php";
                         
                     </div>
                 </div>
-                <!-- <div class="col-lg-3 col-xs-6">
-                    <div class="small-box bg-aqua">
+                <div class="col-lg-3 col-xs-6">
+                    <div class="small-box bg-green">
                         <div class="inner">
                             <h3><?php
-                            $num = 'Rs. 25,000';
+                            $sql = "SELECT id FROM daily_bonus WHERE DATE(datetime) = '$date'";
+                            $db->sql($sql);
+                            $res = $db->getResult();
+                            $num = $db->numRows($res);
                             echo $num;
                              ?></h3>
-                            <p>Unpaid Withdrawals</p>
+                            <p>Active Users</p>
                         </div>
                        
-                        <a href="withdrawals.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                        <a href="users.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                         
                     </div>
-                </div> -->
+                </div>
                 <div class="col-lg-4 col-xs-6">
                     <div class="small-box bg-orange">
                         <div class="inner">
