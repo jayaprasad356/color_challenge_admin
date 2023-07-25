18686e8e6e009e0445e7c0f73ee5e0844d302c32
jayaprasad356 <jayaprasad356@gmail.com>
2023-07-25 10:29:24 +0530

    credit

---
+++ api/credit_coins.php
@@ -0,0 +1,40 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+include_once('../includes/crud.php');
+
+$db = new Database();
+$db->connect();
+
+
+
+if (empty($_POST['user_id'])) {
+    $response['success'] = false;
+    $response['message'] = "User ID is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+if (empty($_POST['coins'])) {
+    $response['success'] = false;
+    $response['message'] = "Coins is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+
+$user_id = $db->escapeString($_POST['user_id']);
+$coins = $db->escapeString($_POST['coins']);
+
+$sql = "UPDATE users SET coins= coins + $coins WHERE id=$user_id";
+$db->sql($sql);
+$response['success'] = true;
+$response['message'] = "Coins Added";
+print_r(json_encode($response));
+
+
+?>
