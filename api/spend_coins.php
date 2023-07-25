1b1f40f7fa1f06b39b722460eaa4f3ed8f68e968
jayaprasad356 <jayaprasad356@gmail.com>
2023-07-25 12:09:51 +0530

    credit

--- api/spend_coins.php
+++ api/spend_coins.php
@@ -6,12 +6,12 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-
+date_default_timezone_set('Asia/Kolkata');
 include_once('../includes/crud.php');
 
 $db = new Database();
 $db->connect();
-
+$datetime = date('Y-m-d H:i:s');
 
 
 if (empty($_POST['user_id'])) {
@@ -35,6 +35,9 @@ $db->sql($sql);
 $res = $db->getResult();
 $balance_coins = $res[0]['coins'];
 if($balance_coins >= $coins){
+    $type = 'find_color_debit';
+    $sql_query = "INSERT INTO transactions (user_id,type,coins,datetime)VALUES('$user_id','$type','$coins','$datetime')";
+    $db->sql($sql_query);
     $sql = "UPDATE users SET coins= coins - $coins WHERE id=$user_id";
     $db->sql($sql);
     $sql = "SELECT coins FROM users WHERE id = '$user_id'";
