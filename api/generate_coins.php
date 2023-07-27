0f977354b9d738a6eb78e87190869667c4a4f360
jayaprasad356 <jayaprasad356@gmail.com>
2023-07-28 00:46:06 +0530

    level

--- api/generate_coins.php
+++ api/generate_coins.php
@@ -129,7 +129,7 @@ if ($num == 1){
         $response['max_coin'] = 100;
         $response['time_left'] = $time_left;
         $response['refer_amount'] = 200;
-        $response['level'] = 1;
+        $response['level'] = 2;
         print_r(json_encode($response));
 
     }
