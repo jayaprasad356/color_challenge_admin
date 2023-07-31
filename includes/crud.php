1ba12875f210dff6068c649b578ea5149077c804
AKraja777 <ashokkumar7hitter@gmail.com>
2023-07-31 10:31:36 +0530

    header.php

--- includes/crud.php
+++ includes/crud.php
@@ -18,9 +18,9 @@ class Database
 
 
     private $db_host = "localhost";  // Change as required
-    private $db_user = "u743445510_cc";  // Change as required
-    private $db_pass = "Color@2022";  // Change as required
-    private $db_name = "u743445510_cc";    // Change as required
+    private $db_user = "root";  // Change as required
+    private $db_pass = "";  // Change as required
+    private $db_name = "color_challenge";    // Change as required
 
     private $con = false; // Check to see if the connection is active
     private $myconn = ""; // This will be our mysqli object
