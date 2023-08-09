a361d51cc9f1a28c560764ae4770b403c722d3c1
jayaprasad356 <jayaprasad356@gmail.com>
2023-08-09 12:15:47 +0530

    cruud

--- includes/crud.php
+++ includes/crud.php
@@ -17,10 +17,11 @@ class Database
      */
 
 
+
     private $db_host = "localhost";  // Change as required
-    private $db_user = "root";  // Change as required
-    private $db_pass = "";  // Change as required
-    private $db_name = "cc";    // Change as required
+    private $db_user = "u743445510_cc";  // Change as required
+    private $db_pass = "Color@2022";  // Change as required
+    private $db_name = "u743445510_cc"; 
 
     // private $db_host = "localhost";  // Change as required
     // private $db_user = "u743445510_cc";  // Change as required
