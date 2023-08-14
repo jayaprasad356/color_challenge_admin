c940d60ab4e24772bbbb996d541d21973f46ce21
jayaprasad356 <jayaprasad356@gmail.com>
2023-08-14 20:06:53 +0530

    trans

--- api/add_main_balance.php
+++ api/add_main_balance.php
@@ -6,7 +6,7 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-
+date_default_timezone_set('Asia/Kolkata');
 include_once('../includes/crud.php');
 
 $db = new Database();
@@ -25,7 +25,7 @@ if (empty($_POST['wallet_type'])) {
     print_r(json_encode($response));
     return false;
 }
-
+$datetime = date('Y-m-d H:i:s');
 $user_id=$db->escapeString($_POST['user_id']);
 $wallet_type = $db->escapeString($_POST['wallet_type']);
 
@@ -49,6 +49,8 @@ if ($num == 1) {
             print_r(json_encode($response));
             return false;
         }
+        $sql = "INSERT INTO transactions (`user_id`,`tyoe`,`datetime`,`amount`) VALUES ($user_id,'basic_wallet','$datetime',$basic_wallet)";
+        $db->sql($sql);
         $sql = "UPDATE users SET balance= balance + basic_wallet,basic_wallet = 0 WHERE id=" . $user_id;
         $db->sql($sql);
 
@@ -66,6 +68,8 @@ if ($num == 1) {
             print_r(json_encode($response));
             return false;
         }
+        $sql = "INSERT INTO transactions (`user_id`,`tyoe`,`datetime`,`amount`) VALUES ($user_id,'premium_wallet','$datetime',$basic_wallet)";
+        $db->sql($sql);
         $sql = "UPDATE users SET balance= balance + premium_wallet,premium_wallet = 0 WHERE id=" . $user_id;
         $db->sql($sql);
     
