e402fb6c959b267f092b2aa8291bcf7d56a38fd7
AKraja777 <ashokkumar7hitter@gmail.com>
2023-08-25 11:39:11 +0530

    withdrawals.php

--- api/withdrawals.php
+++ api/withdrawals.php
@@ -8,13 +8,10 @@ header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 date_default_timezone_set('Asia/Kolkata');
 
-
 include_once('../includes/crud.php');
 
 $db = new Database();
 $db->connect();
-
-
 
 if (empty($_POST['user_id'])) {
     $response['success'] = false;
@@ -29,77 +26,78 @@ if (empty($_POST['amount'])) {
     return false;
 }
 
-
 function isBetween10AMand6PM() {
-    $currentHour = date('H'); // Get the current hour in 24-hour format
-
-    // Convert the time strings to timestamps for comparison
+    $currentHour = date('H');
     $startTimestamp = strtotime('10:00:00');
     $endTimestamp = strtotime('18:00:00');
-
-    // Check if the current hour is after 9 AM and before 10 PM
     return ($currentHour >= date('H', $startTimestamp)) && ($currentHour < date('H', $endTimestamp));
 }
+
 if (!isBetween10AMand6PM()) {
     $response['success'] = false;
     $response['message'] = "Withdrawal time morning 10AM to 6PM";
-    print_r(json_encode($response)); 
+    print_r(json_encode($response));
     return false;
 }
+
 $user_id = $db->escapeString($_POST['user_id']);
 $amount = $db->escapeString($_POST['amount']);
 $datetime = date('Y-m-d H:i:s');
 $dayOfWeek = date('w', strtotime($datetime));
+
 if ($dayOfWeek == 0) {
     $response['success'] = false;
-    $response['message'] = "Sunday Holiday,Come Back Tomorrow";
+    $response['message'] = "Sunday Holiday, Come Back Tomorrow";
     print_r(json_encode($response));
     return false;
-} 
+}
 
 $sql = "SELECT * FROM settings WHERE id=1";
 $db->sql($sql);
 $result = $db->getResult();
-$min_withdrawal=$result[0]['min_withdrawal'];
+$min_withdrawal = $result[0]['min_withdrawal'];
+
 $sql = "SELECT * FROM users WHERE id='$user_id'";
 $db->sql($sql);
 $res = $db->getResult();
-$balance=$res[0]['balance'];
-$account_num=$res[0]['account_num'];
-$earn=$res[0]['earn'];
-$min_withdrawal=$res[0]['min_withdrawal'];
+$balance = $res[0]['balance'];
+$account_num = $res[0]['account_num'];
+$earn = $res[0]['earn'];
+$min_withdrawal = $res[0]['min_withdrawal'];
+$withdrawal_status = $res[0]['withdrawal_status'];
 
+if ($withdrawal_status == '0') {
+    $response['success'] = false;
+    $response['message'] = "Withdrawals are currently disabled for your account.";
+    print_r(json_encode($response));
+    return false;
+}
 
-if($amount >= $min_withdrawal){
-    if($amount <= $balance){
-        if($account_num == ''){
+if ($amount >= $min_withdrawal) {
+    if ($amount <= $balance) {
+        if ($account_num == '') {
             $response['success'] = false;
             $response['message'] = "Please Update Your Bank details";
             print_r(json_encode($response));
             return false;
-        }
-        else{
+        } else {
             $sql = "INSERT INTO withdrawals (`user_id`,`amount`,`status`,`datetime`) VALUES ('$user_id','$amount',0,'$datetime')";
             $db->sql($sql);
-            $sql="UPDATE users SET balance=balance-'$amount',earn=earn+'$amount' WHERE id='$user_id'";
+            $sql = "UPDATE users SET balance=balance-'$amount', earn=earn+'$amount' WHERE id='$user_id'";
             $db->sql($sql);
 
             $response['success'] = true;
-            $response['message'] = "Withdrawal Requested Successfully";
+            $response['message'] = "Withdrawal Requested Successfully. Your withdrawal status is now stable.";
             print_r(json_encode($response));
-    
         }
-       
-    }else{
+    } else {
         $response['success'] = false;
         $response['message'] = "Insufficient Balance";
         print_r(json_encode($response));
     }
+} else {
+    $response['success'] = false;
+    $response['message'] = "Minimum Withdrawal Amount is $min_withdrawal";
+    print_r(json_encode($response));
 }
-else{
-        $response['success'] = false;
-        $response['message'] = "Minimum Withdrawal Amount is $min_withdrawal";
-        print_r(json_encode($response));
-}
-
 ?>
