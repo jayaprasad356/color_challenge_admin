e2c51b22c3bb0547e9658d74f50f18ea2f3b57b0
jayaprasad356 <jayaprasad356@gmail.com>
2023-10-04 00:31:23 +0530

    post

--- api/posts_list.php
+++ api/posts_list.php
@@ -12,7 +12,14 @@ include_once('../includes/crud.php');
 $db = new Database();
 $db->connect();
 
+// if (empty($_POST['user_id'])) {
+//     $response['success'] = false;
+//     $response['message'] = "User ID is Empty";
+//     print_r(json_encode($response));
+//     return false;
+// }
 
+$user_id = 5420;
 
 $sql = "SELECT * FROM `posts` ORDER BY RAND() LIMIT 25";
 $db->sql($sql);
@@ -21,12 +28,26 @@ $num = $db->numRows($res);
 
 if ($num >= 1){
     foreach ($res as $row) {
+        $post_id = $row['id'];
         $temp['id'] = $row['id'];
         $temp['caption'] = $row['caption'];
         $temp['name'] = 'John Cena';
         $temp['image'] = DOMAIN_URL.'upload/post/'.$row['image'];
-        $temp['likes'] = 0;
-        $temp['share_link'] = 'https://admin.colorjobs.site/mypost.php?id='.$row['id'];
+        $temp['likes'] = $row['likes'];
+        $temp['share_link'] = DOMAIN_URL.'mypost.php?id='.$row['id'];
+        $sql = "SELECT * FROM `likes` WHERE user_id = $user_id AND post_id = $post_id";
+        $db->sql($sql);
+        $res = $db->getResult();
+        $num = $db->numRows($res);
+
+        if ($num >= 1){
+            $status = $res[0]['status'];
+            //$temp['user_like'] = $status;
+
+        }else{
+            //$temp['user_like'] = "0";
+
+        }
         $rows[] = $temp;
     }
     $response['success'] = true;
@@ -38,5 +59,4 @@ else{
     $response['success'] = false;
     $response['message'] = "Not found";
     print_r(json_encode($response));
-
 }
