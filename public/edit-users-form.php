458720aeee6f882a21960d1f1aaab440e456ea2e
jayaprasad356 <jayaprasad356@gmail.com>
2023-10-12 14:41:42 +0530

    user

--- public/edit-users-form.php
+++ public/edit-users-form.php
@@ -264,7 +264,7 @@ if (isset($_POST['btnEdit'])) {
                 $total_referrals = 0;
                 $premium_wallet = 0;
                 $old_plan = 0;
-                $ads_time = 27;
+                $ads_time = 20;
             }
     
             $sql_query = "UPDATE users SET mobile='$mobile',earn='$earn',balance='$balance',referred_by='$referred_by',refer_code='$refer_code',withdrawal_status='$withdrawal_status',min_withdrawal='$min_withdrawal',joined_date = '$joined_date',account_num='$account_num', holder_name='$holder_name', bank='$bank', branch='$branch', ifsc='$ifsc', device_id='$device_id', basic_wallet='$basic_wallet', premium_wallet='$premium_wallet', total_ads='$total_ads', today_ads='$today_ads',status=$status,lead_id='$lead_id',support_id='$support_id',branch_id='$branch_id',support_lan='$support_lan',gender='$gender',current_refers='$current_refers',target_refers='$target_refers',plan = '$plan',total_referrals = $total_referrals,ads_time='$ads_time',ads_cost='$ads_cost',old_plan = $old_plan WHERE id = $ID";
