b88dfebe6a3365ef38a1eef0e16792835eec6919
jayaprasad356 <jayaprasad356@gmail.com>
2023-10-16 13:27:17 +0530

    wallet

--- api/wallet.php
+++ api/wallet.php
@@ -171,7 +171,7 @@ if ($num >= 1) {
             if(($sync_unique_id != $t_sync_unique_id) || $t_sync_unique_id == ''){
 
 
-                $sql = "UPDATE users SET reward_ads = reward_ads + 12 WHERE refer_code = '$referred_by' AND status = 1 AND plan = 'A1'";
+                $sql = "UPDATE users SET reward_ads = reward_ads + 12 WHERE refer_code = '$referred_by' AND status = 1 AND plan = 'A1' AND old_plan = 0";
                 $db->sql($sql);
 
 
