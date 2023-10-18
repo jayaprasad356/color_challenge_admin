11e0740a9e0abc1338bff93bd02511f980417ca5
jayaprasad356 <jayaprasad356@gmail.com>
2023-10-19 02:41:21 +0530

    walllet

--- api/wallet.php
+++ api/wallet.php
@@ -146,7 +146,7 @@ if ($num >= 1) {
         $db->sql($sql);
         $tres = $db->getResult();
         $num = $db->numRows($tres);
-        $code_min_sync_time = 60;
+        $code_min_sync_time = 45;
         if ($num >= 1) {
             $t_sync_unique_id = $tres[0]['sync_unique_id'];
             $dt1 = $tres[0]['datetime'];
