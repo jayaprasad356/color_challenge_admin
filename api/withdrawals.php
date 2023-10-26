5ffb78b4509a5dd0bbb147f1be53b18b264199a5
jayaprasad356 <jayaprasad356@gmail.com>
2023-10-26 19:33:58 +0530

    withdrawal

--- api/withdrawals.php
+++ api/withdrawals.php
@@ -33,12 +33,12 @@ function isBetween10AMand6PM() {
     return ($currentHour >= date('H', $startTimestamp)) && ($currentHour < date('H', $endTimestamp));
 }
 
-// if (!isBetween10AMand6PM()) {
-//     $response['success'] = false;
-//     $response['message'] = "Withdrawal time morning 10AM to 6PM";
-//     print_r(json_encode($response));
-//     return false;
-// }
+if (!isBetween10AMand6PM()) {
+    $response['success'] = false;
+    $response['message'] = "Withdrawal time morning 10AM to 6PM";
+    print_r(json_encode($response));
+    return false;
+}
 
 $user_id = $db->escapeString($_POST['user_id']);
 $amount = $db->escapeString($_POST['amount']);
