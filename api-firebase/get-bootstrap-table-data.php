d44fb720a5dbf96407dfbf0ad0b959dca80edec8
jayaprasad356 <jayaprasad356@gmail.com>
2023-11-03 12:40:55 +0530

    mobile

--- api-firebase/get-bootstrap-table-data.php
+++ api-firebase/get-bootstrap-table-data.php
@@ -455,7 +455,7 @@ if (isset($_GET['table']) && $_GET['table'] == 'withdrawals') {
 
     if (isset($_GET['search']) && !empty($_GET['search'])) {
         $search = $db->escapeString($fn->xss_clean($_GET['search']));
-        $where .= "AND u.mobile like '%' ";
+        $where .= "AND u.mobile like '%" . $search . "%' ";
     }
     if (isset($_GET['sort'])){
         $sort = $db->escapeString($_GET['sort']);
