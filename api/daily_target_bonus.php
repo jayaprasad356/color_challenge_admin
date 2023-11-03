0c7aa88986a40ef9dc9c98e09d500a2cf5996d7d
jayaprasad356 <jayaprasad356@gmail.com>
2023-11-04 00:55:05 +0530

    daily

---
+++ api/daily_target_bonus.php
@@ -0,0 +1,44 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+date_default_timezone_set('Asia/Kolkata');
+include_once('../includes/crud.php');
+
+$db = new Database();
+$db->connect();
+$datetime = date('Y-m-d H:i:s');
+
+
+$sql = "SELECT * FROM `users` WHERE total_referrals >= 5 AND status = 1 AND old_plan = 0";
+$db->sql($sql);
+$res= $db->getResult();
+$num = $db->numRows($res);
+if ($num >= 1){
+    
+    foreach ($res as $row) {
+        $ID = $row['id'];
+        $ads = 1200;
+        $type = 'target_bonus';
+        $per_code_cost = 0.125;
+        $amount = $ads * $per_code_cost;
+        $sql = "INSERT INTO transactions (`user_id`,`ads`,`amount`,`datetime`,`type`)VALUES('$ID','$ads','$amount','$datetime','$type')";
+        $db->sql($sql);
+        $sql = "UPDATE `users` SET  `today_ads` = today_ads + $ads,`total_ads` = total_ads + $ads,`earn` = earn + $amount,`balance` = balance + $amount WHERE `id` = $ID";
+        $db->sql($sql);
+    }
+    $response['success'] = true;
+    $response['message'] = "Target  Successfully";
+    print_r(json_encode($response));
+}
+else{
+    $response['success'] = false;
+    $response['message'] = "Users Not found";
+    print_r(json_encode($response));
+
+}
+?>
