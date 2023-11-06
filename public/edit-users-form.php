a5356828b916d631f4090b3a2c91c1453e55d473
AKraja777 <ashokkumar7hitter@gmail.com>
2023-11-06 10:00:38 +0530

    edit-users-form.php

--- public/edit-users-form.php
+++ public/edit-users-form.php
@@ -56,6 +56,7 @@ if (isset($_POST['btnEdit'])) {
     $worked_days = $db->escapeString(($_POST['worked_days']));
     $description = $db->escapeString(($_POST['description']));
     $age = $db->escapeString(($_POST['age']));
+    $project_type = $db->escapeString(($_POST['project_type']));
 
     
     $error = array();
@@ -275,7 +276,7 @@ if (isset($_POST['btnEdit'])) {
                 $ads_time = 20;
             }
     
-            $sql_query = "UPDATE users SET mobile='$mobile',earn='$earn',balance='$balance',referred_by='$referred_by',refer_code='$refer_code',withdrawal_status='$withdrawal_status',min_withdrawal='$min_withdrawal',joined_date = '$joined_date',account_num='$account_num', holder_name='$holder_name', bank='$bank', branch='$branch', ifsc='$ifsc', device_id='$device_id', basic_wallet='$basic_wallet', premium_wallet='$premium_wallet', total_ads = $total_ads, today_ads = $today_ads,status=$status,lead_id='$lead_id',support_id='$support_id',branch_id='$branch_id',support_lan='$support_lan',gender='$gender',current_refers='$current_refers',target_refers='$target_refers',plan = '$plan',total_referrals = $total_referrals,ads_time='$ads_time',ads_cost='$ads_cost',old_plan = '$old_plan',worked_days = '$worked_days',blocked = '$blocked',description = '$description',age = '$age' WHERE id = $ID";
+            $sql_query = "UPDATE users SET mobile='$mobile',earn='$earn',balance='$balance',referred_by='$referred_by',refer_code='$refer_code',withdrawal_status='$withdrawal_status',min_withdrawal='$min_withdrawal',joined_date = '$joined_date',account_num='$account_num', holder_name='$holder_name', bank='$bank', branch='$branch', ifsc='$ifsc', device_id='$device_id', basic_wallet='$basic_wallet', premium_wallet='$premium_wallet', total_ads = $total_ads, today_ads = $today_ads,status=$status,lead_id='$lead_id',support_id='$support_id',branch_id='$branch_id',support_lan='$support_lan',gender='$gender',current_refers='$current_refers',target_refers='$target_refers',plan = '$plan',total_referrals = $total_referrals,ads_time='$ads_time',ads_cost='$ads_cost',old_plan = '$old_plan',worked_days = '$worked_days',blocked = '$blocked',description = '$description',age = '$age',project_type = '$project_type' WHERE id = $ID";
             $db->sql($sql_query);
             $update_result = $db->getResult();
     
@@ -663,6 +664,14 @@ if (isset($_POST['btnCancel'])) { ?>
                                     <label for="exampleInputEmail1">Age</label> <i class="text-danger asterik">*</i><?php echo isset($error['age']) ? $error['age'] : ''; ?>
                                     <input type="text" class="form-control" name="age" value="<?php echo $res[0]['age']; ?>">
                                 </div>
+                                <div class="col-md-3">
+                                   <label for="exampleInputEmail1">Project Type</label> <i class="text-danger asterik">*</i>
+                                    <select id='project_type' name="project_type" class='form-control'>
+                                     <option value='regular' <?php if ($res[0]['project_type'] == 'regular') echo 'selected'; ?>>regular</option>
+                                      <option value='rejoin' <?php if ($res[0]['project_type'] == 'rejoin') echo 'selected'; ?>>rejoin</option>
+                                      <option value='free' <?php if ($res[0]['project_type'] == 'free') echo 'selected'; ?>>free</option>
+                                    </select>
+                                    </div>
                     </div><!-- /.box-body -->
                 </form>
             </div><!-- /.box -->
