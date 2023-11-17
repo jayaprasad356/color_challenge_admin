338e70180dbb4e2e17365ee64084f17bb1420027
jayaprasad356 <jayaprasad356@gmail.com>
2023-11-17 23:15:43 +0530

    plan expired

--- api/wallet.php
+++ api/wallet.php
@@ -132,6 +132,13 @@ if ($num >= 1) {
         return false;
 
     }
+    if($worked_days >= 30){
+        $response['success'] = false;
+        $response['message'] = "Your Plan Expired";
+        print_r(json_encode($response));
+        return false;
+
+    }
 
 
 
