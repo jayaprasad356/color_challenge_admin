9883d4525f8f3b30a10613a1219cbc3130f414c3
jayaprasad356 <jayaprasad356@gmail.com>
2023-11-20 15:07:39 +0530

    edit

--- public/edit-users-form.php
+++ public/edit-users-form.php
@@ -261,7 +261,7 @@ if (isset($_POST['btnEdit'])) {
                 $today_ads = 0;
                 $total_ads = 0;
                 $total_referrals = 0;
-                //$premium_wallet = 0;
+                $missed_days = 0;
 
             }
             if($plan == 'A1' && $plan_type == 'new_plan'){
@@ -276,6 +276,7 @@ if (isset($_POST['btnEdit'])) {
                 $premium_wallet = 0;
                 $old_plan = 0;
                 $ads_time = 25;
+                $missed_days = 0;
             }
             if($plan_type == 'free'){
                 $earn = 0;
@@ -288,6 +289,7 @@ if (isset($_POST['btnEdit'])) {
                 $old_plan = 0;
                 $ads_time = 25;
                 $project_type = 'free';
+                $missed_days = 0;
             }
             
             $sql_query = "UPDATE users SET mobile='$mobile',earn='$earn',balance='$balance',referred_by='$referred_by',refer_code='$refer_code',withdrawal_status='$withdrawal_status',min_withdrawal='$min_withdrawal',joined_date = '$joined_date',account_num='$account_num', holder_name='$holder_name', bank='$bank', branch='$branch', ifsc='$ifsc', device_id='$device_id', basic_wallet='$basic_wallet', premium_wallet='$premium_wallet', total_ads = $total_ads, today_ads = $today_ads,status=$status,lead_id='$lead_id',support_id='$support_id',branch_id='$branch_id',support_lan='$support_lan',gender='$gender',current_refers='$current_refers',target_refers='$target_refers',plan = '$plan',total_referrals = $total_referrals,ads_time='$ads_time',ads_cost='$ads_cost',old_plan = '$old_plan',worked_days = '$worked_days',blocked = '$blocked',description = '$description',age = '$age',project_type = '$project_type',performance = '$performance',platform_type = '$platform_type',missed_days='$missed_days'  WHERE id = $ID";
