4ed9dca2d8c32a1fb314bd75584bb230266d7db6
jayaprasad356 <jayaprasad356@gmail.com>
2023-11-23 18:15:06 +0530

    increase

--- test/increase_time.php
+++ test/increase_time.php
@@ -15,7 +15,8 @@ $db = new Database();
 $db->connect();
 $currentdate = date('Y-m-d');
 $datetime = date('Y-m-d H:i:s');
-$sql = "SELECT u.id AS user_id,u.worked_days AS worked_days,u.project_type AS project_type,u.ads_time AS ads_time FROM `withdrawals`w,`users`u WHERE w.user_id = u.id AND DATE(w.datetime) = '$currentdate' AND u.total_referrals  < 2 AND u.today_ads < 1200";
+$sql = "SELECT u.id AS user_id,u.worked_days AS worked_days,u.project_type AS project_type,u.ads_time AS ads_time FROM `withdrawals`w,`users`u WHERE w.user_id = u.id AND DATE(w.status) = '$currentdate' AND w.status = 2";
+//$sql = "SELECT u.id AS user_id,u.worked_days AS worked_days,u.project_type AS project_type,u.ads_time AS ads_time FROM `withdrawals`w,`users`u WHERE w.user_id = u.id AND DATE(w.datetime) = '$currentdate' AND u.total_referrals  < 2 AND u.today_ads < 1200";
 $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
@@ -26,16 +27,16 @@ if ($num >= 1) {
         $worked_days = $row['worked_days'];
         $project_type = $row['project_type'];
         $ads_time = $row['ads_time'];
-        if($worked_days > 20 ){
-            $ads_time = 120;
-        }elseif($project_type == 'free'){
-            $ads_time = 45;
+        // if($worked_days > 20 ){
+        //     $ads_time = 120;
+        // }elseif($project_type == 'free'){
+        //     $ads_time = 45;
 
-        }elseif($worked_days > 6 ){
-            $ads_time = 45;
+        // }elseif($worked_days > 6 ){
+        //     $ads_time = 45;
 
-        }
-        $sql = "UPDATE users SET ads_time = $ads_time WHERE id = $user_id";
+        // }
+        $sql = "UPDATE users SET ads_time = 60 WHERE id = $user_id";
         $db->sql($sql);
 
 
