bbd791fe4faa7917822b1f96883aa09b1da4b710
jayaprasad356 <jayaprasad356@gmail.com>
2023-11-23 16:55:27 +0530

    cancel

--- test/cancel_withdrawal.php
+++ test/cancel_withdrawal.php
@@ -15,7 +15,7 @@ $db = new Database();
 $db->connect();
 $currentdate = date('Y-m-d');
 $datetime = date('Y-m-d H:i:s');
-$sql = "SELECT w.id AS w_id FROM `withdrawals`w,`users`u WHERE w.user_id = u.id AND w.status = 0 AND u.total_referrals < 2 AND u.worked_days > 5 AND u.plan = 'A1' AND u.today_ads < 1200";
+$sql = "SELECT w.id AS w_id,u.worked_days AS worked_days,u.total_referrals AS total_referrals FROM `withdrawals`w,`users`u WHERE w.user_id = u.id AND w.status = 0 AND u.total_referrals < 2 AND u.worked_days > 5 AND u.plan = 'A1' AND u.today_ads < 1200";
 $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
@@ -23,16 +23,22 @@ if ($num >= 1) {
 
     foreach ($res as $row) {
         $w_id = $row['w_id'];
-        $sql = "UPDATE withdrawals SET status=2 WHERE id = $w_id";
-        $db->sql($sql);
-        $sql = "SELECT * FROM `withdrawals` WHERE id = $w_id ";
-        $db->sql($sql);
-        $res = $db->getResult();
-        $user_id= $res[0]['user_id'];
-        $amount= $res[0]['amount'];
+        $worked_days = $row['worked_days'];
+        $total_referrals = $row['total_referrals'];
+        if(($worked_days > 5 && $total_referrals < 1) || ($worked_days > 11 && $total_referrals < 2)){
+            $sql = "UPDATE withdrawals SET status=2 WHERE id = $w_id";
+            $db->sql($sql);
+            $sql = "SELECT * FROM `withdrawals` WHERE id = $w_id ";
+            $db->sql($sql);
+            $res = $db->getResult();
+            $user_id= $res[0]['user_id'];
+            $amount= $res[0]['amount'];
+    
+            $sql = "UPDATE users SET balance= balance + $amount WHERE id = $user_id";
+            $db->sql($sql);
 
-        $sql = "UPDATE users SET balance= balance + $amount WHERE id = $user_id";
-        $db->sql($sql);
+        }
+
 
 
     }
