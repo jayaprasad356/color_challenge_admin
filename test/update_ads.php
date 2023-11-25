e563f1bc791bbbf524f4e29e8a9b31002a1ba786
jayaprasad356 <jayaprasad356@gmail.com>
2023-11-26 01:48:32 +0530

    up

--- test/update_ads.php
+++ test/update_ads.php
@@ -6,16 +6,16 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-
+date_default_timezone_set('Asia/Kolkata');
 include_once('../includes/crud.php');
 include_once('../includes/functions.php');
 $fnc = new functions;
 $db = new Database();
 $db->connect();
+$currentdate = date('Y-m-d');
 
 
-
-$sql = "SELECT id,joined_date FROM `users` WHERE old_plan = 0 AND plan = 'A1' AND status = 1 AND worked_days = 11 AND total_referrals = 0";
+$sql = "SELECT id FROM `users` WHERE worked_days > 0 AND status = 1 AND plan = 'A1' AND old_plan = 0";
 $db->sql($sql);
 $res= $db->getResult();
 $num = $db->numRows($res);
@@ -23,24 +23,12 @@ if ($num >= 1){
     
     foreach ($res as $row) {
         $user_id = $row['id'];
-        $joined_date = $row['joined_date']; // Assuming $joined_date is a date strin
-        $date = new DateTime($joined_date);
-        $date->add(new DateInterval('P10D'));
-        $new_date = $date->format('Y-m-d'); // Change the format as needed
-        $sql = "SELECT COUNT(*) AS leaves
-        FROM `leaves`
-        WHERE date BETWEEN '$joined_date' AND '$new_date'";
-        $this->db->sql($sql);
-        $res = $this->db->getResult();
-        $leaves = $res[0]['leaves'];
-        $total_days = 10 + $leaves;
-
-        $sql = "SELECT SUM(ads) AS total_ads FROM `transactions` WHERE DATE(datetime) >= '$joined_date' AND DATE(datetime) <= DATE_ADD('$joined_date', INTERVAL $total_days DAY)  AND user_id = $user_id";
+        $sql = "SELECT SUM(ads) AS total_ads FROM `transactions` WHERE DATE(datetime) = '$currentdate' AND user_id = $user_id";
         $db->sql($sql);
         $res= $db->getResult();
         $total_ads = ($res[0]['total_ads'] !== null) ? $res[0]['total_ads'] : 0;
 
-        $sql = "UPDATE users SET ads_10th_day = $total_ads WHERE id = $user_id";
+        $sql = "UPDATE users SET last_today_ads = $total_ads WHERE id = $user_id";
         $db->sql($sql);
 
     
