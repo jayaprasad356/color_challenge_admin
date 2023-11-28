9c77d9211e8ba4ed93df4f4853acce622e635fdf
jayaprasad356 <jayaprasad356@gmail.com>
2023-11-28 18:09:21 +0530

    cancel

--- test/cancel_withdrawal.php
+++ test/cancel_withdrawal.php
@@ -15,31 +15,19 @@ $db = new Database();
 $db->connect();
 $currentdate = date('Y-m-d');
 $datetime = date('Y-m-d H:i:s');
-$sql = "SELECT w.id AS w_id,u.worked_days AS worked_days,u.total_referrals AS total_referrals FROM `withdrawals`w,`users`u WHERE w.user_id = u.id AND w.status = 0 AND u.total_referrals < 2 AND u.worked_days > 5 AND u.plan = 'A1' AND u.today_ads < 1200";
+$sql = "SELECT w.id AS w_id,w.amount AS amount,u.id AS user_id FROM `withdrawals`w,`users`u WHERE w.user_id = u.id AND DATE(w.datetime) = '$currentdate' AND u.total_referrals  < 2 AND u.today_ads < 1200 AND u.worked_days > 5";
 $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
 if ($num >= 1) {
-
     foreach ($res as $row) {
         $w_id = $row['w_id'];
-        $worked_days = $row['worked_days'];
-        $total_referrals = $row['total_referrals'];
-        if(($worked_days > 5 && $total_referrals < 1) || ($worked_days > 11 && $total_referrals < 2)){
-            $sql = "UPDATE withdrawals SET status=2 WHERE id = $w_id";
-            $db->sql($sql);
-            $sql = "SELECT * FROM `withdrawals` WHERE id = $w_id ";
-            $db->sql($sql);
-            $res = $db->getResult();
-            $user_id= $res[0]['user_id'];
-            $amount= $res[0]['amount'];
-    
-            $sql = "UPDATE users SET balance= balance + $amount WHERE id = $user_id";
-            $db->sql($sql);
-
-        }
-
-
+        $amount = $row['amount'];
+        $user_id = $row['user_id'];
+        $sql = "UPDATE withdrawals SET status=2 WHERE id = $w_id";
+        $db->sql($sql);
+        $sql = "UPDATE users SET balance= balance + $amount WHERE id = $user_id";
+        $db->sql($sql);
 
     }
     $response['success'] = true;
--- test/increase_time.php
+++ test/increase_time.php
@@ -16,7 +16,7 @@ $db->connect();
 $currentdate = date('Y-m-d');
 $datetime = date('Y-m-d H:i:s');
 //$sql = "SELECT u.id AS user_id,u.worked_days AS worked_days,u.project_type AS project_type,u.ads_time AS ads_time FROM `withdrawals`w,`users`u WHERE w.user_id = u.id AND DATE(w.datetime) = '$currentdate' AND w.status = 2";
-$sql = "SELECT u.id FROM `withdrawals`w,`users`u WHERE w.user_id = u.id AND DATE(w.datetime) = '$currentdate' AND u.total_referrals  < 2 AND u.today_ads < 1200 AND u.worked_days > 7";
+$sql = "SELECT u.id AS user_id FROM `withdrawals`w,`users`u WHERE w.user_id = u.id AND DATE(w.datetime) = '$currentdate' AND u.total_referrals  < 2 AND u.today_ads < 1200 AND u.worked_days > 5";
 $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
@@ -24,18 +24,6 @@ if ($num >= 1) {
 
     foreach ($res as $row) {
         $user_id = $row['user_id'];
-        $worked_days = $row['worked_days'];
-        $project_type = $row['project_type'];
-        $ads_time = $row['ads_time'];
-        // if($worked_days > 20 ){
-        //     $ads_time = 120;
-        // }elseif($project_type == 'free'){
-        //     $ads_time = 45;
-
-        // }elseif($worked_days > 6 ){
-        //     $ads_time = 45;
-
-        // }
         $sql = "UPDATE users SET ads_time = 60 WHERE id = $user_id";
         $db->sql($sql);
 
