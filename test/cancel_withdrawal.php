45e89c25d78045b7d824e826e1c7ac36d88c8bf6
jayaprasad356 <jayaprasad356@gmail.com>
2023-12-02 17:43:50 +0530

    can

--- test/cancel_withdrawal.php
+++ test/cancel_withdrawal.php
@@ -15,7 +15,7 @@ $db = new Database();
 $db->connect();
 $currentdate = date('Y-m-d');
 $datetime = date('Y-m-d H:i:s');
-$sql = "SELECT w.id AS w_id,w.amount AS amount,u.id AS user_id WHERE w.user_id = u.id AND DATE(datetime) = '2023-12-02'  AND w.status = 0  AND  u.plan = 'A1' AND u.status = 1 AND project_type = 'free'";
+$sql = "SELECT w.id AS w_id,w.amount AS amount,u.id AS user_id FROM `withdrawals`w,`users`u WHERE w.user_id = u.id AND DATE(datetime) = '2023-12-02'  AND w.status = 0  AND  u.plan = 'A1' AND u.status = 1 AND project_type = 'free'";
 $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
