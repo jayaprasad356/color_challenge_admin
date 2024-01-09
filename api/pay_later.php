f22cfe7c3400ec5c06bde32f454fad6288be429a
jayaprasad356 <jayaprasad356@gmail.com>
2024-01-10 01:42:33 +0530

    pay later

---
+++ api/pay_later.php
@@ -0,0 +1,58 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+date_default_timezone_set('Asia/Kolkata');
+include_once('../includes/crud.php');
+
+$db = new Database();
+$db->connect();
+$datetime = date('Y-m-d H:i:s');
+$currentdate = date('Y-m-d');
+
+$sql = "SELECT * FROM leaves WHERE date = '$currentdate'";
+$db->sql($sql);
+$resl = $db->getResult();
+$lnum = $db->numRows($resl);
+$enable = 1;
+if ($lnum >= 1) {
+
+    return false;
+
+}
+$sql = "SELECT * FROM users WHERE status = 1 AND without_work = 1 AND plan = 'A1U' AND pay_later > 0";
+$db->sql($sql);
+$res= $db->getResult();
+$num = $db->numRows($res);
+if ($num >= 1){
+    
+    foreach ($res as $row) {
+        $ID = $row['id'];
+        $amount = -10;
+        $ads = 0;
+        $datetime = date('Y-m-d H:i:s');
+        $type = 'pay_later';
+        $sql = "INSERT INTO transactions (`user_id`,`ads`,`amount`,`datetime`,`type`)VALUES('$ID','$ads','$amount','$datetime','$type')";
+        $db->sql($sql);
+        $res = $db->getResult();
+    
+        $sql = "UPDATE `users` SET  `earn` = earn + $amount,`balance` = balance + $amount,`pay_later` = pay_later + $amount WHERE `id` = $ID";
+        $db->sql($sql);
+        $result = $db->getResult();
+
+    }
+    $response['success'] = true;
+    $response['message'] = "Ad bonus  added Successfully";
+    print_r(json_encode($response));
+}
+else{
+    $response['success'] = false;
+    $response['message'] = "Users Not found";
+    print_r(json_encode($response));
+
+}
+?>
