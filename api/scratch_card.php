680aeadf5f5b54bdfe4f88b3ecfffe9adaa1af52
AKraja777 <ashokkumar7hitter@gmail.com>
2024-01-30 13:12:26 +0530

    scratch_card.php

---
+++ api/scratch_card.php
@@ -0,0 +1,50 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-Type: application/json");
+header("Expires: 0");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+include_once('../includes/crud.php');
+$db = new Database();
+$db->connect();
+
+if (empty($_POST['user_id'])) {
+    $response['success'] = false;
+    $response['message'] = "User Id is Empty";
+    print_r(json_encode($response));
+    return false;
+}
+
+$user_id = $db->escapeString($_POST['user_id']);
+
+
+$sql = "SELECT * FROM users WHERE id = $user_id ";
+$db->sql($sql);
+$user = $db->getResult();
+
+if (empty($user)) {
+    $response['success'] = false;
+    $response['message'] = "User not found";
+    print_r(json_encode($response));
+    return false;
+}
+
+$sql = "SELECT * FROM users WHERE id = $user_id AND scratch_card = 1";
+$db->sql($sql);
+$res = $db->getResult();
+$num = $db->numRows($res);
+if($num>=1){
+    $response['success'] = true;
+    $response['message'] = "Scratch Card is Available For This User";
+    $response['data'] = $res;
+    print_r(json_encode($response));
+}
+else{
+    $response['success'] = false;
+    $response['message'] = "Scratch Card is Not Available For This User";
+    print_r(json_encode($response));
+
+}
