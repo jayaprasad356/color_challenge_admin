e316d8509fd997545a3e5d81c15c310fcab1517c
ashok3221 <ashokkumarraja77712@gmail.com>
2024-03-08 19:43:51 +0530

    upload_status.php

--- api/upload_status.php
+++ api/upload_status.php
@@ -6,16 +6,13 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
+date_default_timezone_set('Asia/Kolkata');
 
 include_once('../includes/crud.php');
 include_once('../includes/custom-functions.php');
-include_once('../includes/functions.php');
-
+$fn = new custom_functions;
 $db = new Database();
 $db->connect();
-$fn = new functions();
-
-$response = array();
 
 if (empty($_POST['user_id'])) {
     $response['success'] = false;
