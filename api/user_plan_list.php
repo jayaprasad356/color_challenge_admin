48118a365f14f1fe82db91551fa4364c2cde24a7
jayaprasad356 <jayaprasad356@gmail.com>
2024-03-13 21:44:03 +0530

    user

--- api/user_plan_list.php
+++ api/user_plan_list.php
@@ -6,9 +6,9 @@ header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-
+date_default_timezone_set('Asia/Kolkata');
 include_once('../includes/crud.php');
-
+$currentdate = date('Y-m-d');
 $db = new Database();
 $db->connect();
 
@@ -30,11 +30,36 @@ $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
 
+function remaining_days($start_date, $end_date) {
+    $start = new DateTime($start_date);
+    $end = new DateTime($end_date);
+    $remaining = $end->diff($start);
+    return $remaining->format('%a');
+}
+
 if ($num >= 1) {
     foreach ($res as &$job) {
         $imagePath = $job['image'];
+        $id = $job['id'];
         $imageURL = DOMAIN_URL . $imagePath;
         $job['image'] = $imageURL;
+        $joined_date = $job['joined_date'];
+        
+        $sql = "SELECT COUNT(id) AS count FROM leaves WHERE date >= '$joined_date'  AND date <= '$currentdate'";
+        $db->sql($sql);
+        $res1 = $db->getResult();
+        $num = $db->numRows($res1);
+        $remaining = remaining_days($joined_date, $currentdate);
+
+        if($id == 1){
+            $total = 30;
+        }else if($id == 2){
+            $total = 60;
+        }else{
+            $total = 90;
+        }
+
+        $job['remaining_days'] = (string)($total - ($remaining - $num));
     }
 
     $response['success'] = true;
